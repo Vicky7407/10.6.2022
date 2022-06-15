5d019d5cdedcd1af77aa1d41f22d41deae4c1c9a
vicky7407 <vinayrathod5475@gmil.com>
2022-06-15 14:45:45 +0530

    city hospital

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from "./Component/Header"
-import Footer from "./Component/Footer"
-// import Home from "./Container/Home"
-
+import Header from './Container/Header';
+import Home from './Container/Home';
+import Department from './Container/Department';
+import Footer from './Container/Footer';
+import { Switch, Route } from 'react-router-dom';
+import Doctors from './Container/Doctors';
+import About from './Container/About';
+import Contact from './Container/Contact';
+import Appoinment from './Container/Appoinment';
+import Login from './Container/Login';
 
 function App() {
   return (
-    <>
-     <Header />
-     {/* <Home /> */}
-     <Footer />
-     </>
-    
+    <div className="App">
+      <Header/>
+      <Switch>
+        
+        <Route exact path={"/"} ><Home/></Route>
+        <Route exact path="/department" ><Department/></Route>
+        <Route exact path="/doctors" ><Doctors/></Route>
+        <Route exact path="/about" ><About/></Route>
+        <Route exact path="/contact" ><Contact/></Route>
+        <Route exact path="/appointment" ><Appoinment/></Route>
+        <Route exact path="/login" ><Login/></Route>
+        
+      </Switch>
+      <Footer/>
+    </div>
   );
 }
 
